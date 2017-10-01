30983b6b1df53682660cf48fa70f0470d72e9510
tanksforthememories <s4928937@bournemouth.ac.uk>
2017-10-02 00:37:52 +0100

    Half way through problem 3

--- src/main/java/uk/ac/bmth/aprog/task01/Task01.java
+++ src/main/java/uk/ac/bmth/aprog/task01/Task01.java
@@ -8,11 +8,9 @@ public class Task01 {
     /**
      * If we list all the natural numbers below 10 that are multiples of 3 or 5,
      * we get 3, 5, 6 and 9. The sum of these multiples is 23.
-     * <p>
      * Find the sum of all the multiples of 3 or 5 below 1000.
      */
-
-  /*  private static void problem1() {
+    private static void problem1() {
         System.out.println("Problem 1:");
         System.out.println("==========");
 
@@ -26,8 +24,8 @@ public class Task01 {
             }
         }
         System.out.println("Sum: " + Integer.toString(sum));
-    } */
-/*
+    }
+
     /**
      * Write a program that that asks for a number to be input, and then lists
      * all prime numbers up to that number.
@@ -46,58 +44,67 @@ public class Task01 {
         usrInput.close();
 
         for (int i = 1; i <= usrNum; i++) {
-            count =0;
+            count = 0;
             for (int j = i; j >= 1; j--) {
                 if (i % j == 0) {
-                  count++;
+                    count++;
                 }
             }
             if (count == 2) {
                 primeNumbers = primeNumbers + i + " ";
             }
         }
-
-            System.out.println("The prime numbers up to your integer are: ");
-            System.out.println(primeNumbers);
+        System.out.println("The prime numbers up to your integer are: ");
+        System.out.println(primeNumbers);
     }
 
-        /**
-         * A gambling company requires that you select six different numbers from
-         * the integers 1 to 49 to make up a full lottery number.  Each number can
-         * only appear once in a given lottery number.  Write a program to do this and
-         * use it to generate five lottery numbers (ie five sets of 6 numbers).
-         */
-  /*  private static void problem3() {
+    /**
+     * A gambling company requires that you select six different numbers from
+     * the integers 1 to 49 to make up a full lottery number.  Each number can
+     * only appear once in a given lottery number.  Write a program to do this and
+     * use it to generate five lottery numbers (ie five sets of 6 numbers).
+     */
+    private static void problem3() {
         System.out.println("Problem 3:");
         System.out.println("==========");
 
-        // TODO Implement problem 3
+        Scanner usrInput = new Scanner(System.in);
+        int[] usrNum = new int[6];
 
+        System.out.println("Welcome to Andreas' Lottery!");
+        System.out.println("Please select 6 numbers between 1 and 49 to enter the lottery");
+
+        for (int i = 0; i < usrNum.length; i++) {
+            System.out.print("Select a number: ");
+            usrNum[i] = usrInput.nextInt();
+        }
+        for (int i = 0; i < usrNum.length; i++) {
+            System.out.print(usrNum[i] + " ");
+        }
         // output should be a list of five lottery numbers, each containing 6 integers
     }
 
-    *//**
-         * Write a program to display a rectangular multiplication table.  It should
-         * look like the example below.  Do not just use a succession of hard-coded
-         * output statements – you should be using loops.
-         *
-         * @see {@link PrintStream#printf(String, Object...)}
-         *//*
+    /**
+     * Write a program to display a rectangular multiplication table.  It should
+     * look like the example below.  Do not just use a succession of hard-coded
+     * output statements – you should be using loops.
+     *
+     * @see {@link PrintStream#printf(String, Object...)}
+     */
     private static void problem4() {
         System.out.println("Problem 4:");
         System.out.println("==========");
 
-        // TODO Implement problem 4
 
         // output should be a table of numbers (make sure they line up properly
     }
 
 
-    *//**
-         * Write a program that asks you to input a short sentence, and then displays
-         * it having reversed the sequence of letters in each word of your chosen
-         * sentence of text.  For instance, "To be or not to be." becomes "oT eb ro ton ot eb."
-         *//*
+    /**
+     * Write a program that asks you to input a short sentence, and then displays
+     * it having reversed the sequence of letters in each word of your chosen
+     * sentence of text.For instance, "To be or not to be."becomes "oT eb ro ton ot eb."
+     */
     private static void problem5() {
         System.out.println("Problem 5:");
         System.out.println("==========");
@@ -106,37 +113,34 @@ public class Task01 {
     }
 
 
-    */
-
-        /**
-         * Write a program which will ask the user to input a value for min, and a value for max,
-         * and also a paragraph of text.  It should then output a list of all of the sentences
-         * within the paragraph, along with the length of that sentence, where their length is:
-         * min <= length <= max.
-         * It should also output the number of proper characters, the number of (actual) spaces
-         * and also the number of punctuation symbols for the whole paragraph.
-         * <p>
-         * •	Assume that a sentence ends in a period, question mark, or exclamation point.
-         * •	Assume that the sentence does not contain any quotation marks (apostrophes are allowed).
-         * •	The length of the sentence includes all spaces and punctuation.
-         * •	Min and Max should be positive integers less than 1000, and Min <= Max.  The program should check for this.
-         *//*
+    /**
+     * Write a program which will ask the user to input a value for min, and a value for max,
+     * and also a paragraph of text.  It should then output a list of all of the sentences
+     * within the paragraph, along with the length of that sentence, where their length is:
+     * min <= length <= max.
+     * It should also output the number of proper characters, the number of (actual) spaces
+     * and also the number of punctuation symbols for the whole paragraph.
+     * •	Assume that a sentence ends in a period, question mark, or exclamation point.
+     * •	Assume that the sentence does not contain any quotation marks (apostrophes are allowed).
+     * •	The length of the sentence includes all spaces and punctuation.
+     * •	Min and Max should be positive integers less than 1000, and Min <= Max.  The program should check for this.
+     */
     private static void problem6() {
         System.out.println("Problem 6:");
         System.out.println("==========");
 
         // TODO Implement problem 6
 
-    }*/
+    }
 
     public static void main(String[] args) {
-       // problem1();
+        // problem1();
         System.out.println();
-        problem2();
+        //problem2();
         System.out.println();
-       // problem3();
+        problem3();
         System.out.println();
-       // problem4();
+        // problem4();
         System.out.println();
         //problem5();
         System.out.println();
